039f90c8d3ef98abfea5d37912db7a44b72aa06e
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-02 16:47:06 -0800

    Added file 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     ch = ""
-    for i in range(1,101):
+    for i in range(1, 101):
         if i <= 99:
             if i % 3 == 0 and i % 5 == 0:
                 ch += "FizzBuzz "
@@ -21,4 +21,3 @@ def fizzbuzz():
             else:
                 ch += str(i)
     print("{}".format(ch), end=" ")
-
