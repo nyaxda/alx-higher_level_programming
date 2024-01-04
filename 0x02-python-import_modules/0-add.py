4b02bf2658cf9c0e8bbf380c5ba89df600bfceac
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-04 05:17:07 -0600

    Corrected my code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
-sum = add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+def main():
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
+
+if __name__ = "__main__":
+    main()
