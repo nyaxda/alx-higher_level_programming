ba1bfbcc6bce89971a302ad0ae4eaa133c821ef6
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-04 05:41:10 -0800

    Added 4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def main():
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] == "__":
+            continue
+        else:
+            print(name)
+
+
+if __name__ == "__main__":
+    main()
