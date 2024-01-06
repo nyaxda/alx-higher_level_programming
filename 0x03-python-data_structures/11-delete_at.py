12306329d3215767f4c47c680d1a96941dbf3482
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-06 11:09:28 -0800

    Code correction file 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list is None or not my_list:
+    if my_list is None:
         return None
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
