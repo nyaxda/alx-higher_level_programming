792fa852f2a3c1f199bd51be00879c021812ea3a
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-06 02:50:21 -0800

    Corrected file 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if my_list is None:
         return None
-    elif idx < 0 or idx > len(my_list):
+    elif idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
         b = my_list.copy()
