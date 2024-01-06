14c5061415695cb8c1fb0f56f798b3372edee029
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-06 03:35:09 -0800

    Added 7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 1:
+        a = tuple_a[0]
+        b = 0
+    elif not tuple_a or tuple_a is None:
+        a = 0
+        b = 0
+    else:
+        a, b = tuple_a
+    if len(tuple_b) == 1:
+        c = tuple_b[0]
+        d = 0
+    elif not tuple_b or tuple_b is None:
+        c = 0
+        d = 0
+    else:
+        c, d = tuple_b
+    tuple_c = (a+c, b+d)
+    return (tuple_c)
