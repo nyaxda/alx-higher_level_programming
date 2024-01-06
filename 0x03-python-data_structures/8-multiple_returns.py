81f2887a408d0bc2e189d2cbee3ccc42b9b57842
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-06 08:35:18 -0800

    Added 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence is None:
+        a = None
+    else:
+        a = len(sentence)
+        b = sentence[0]
+        c = (a, b)
+    return c
