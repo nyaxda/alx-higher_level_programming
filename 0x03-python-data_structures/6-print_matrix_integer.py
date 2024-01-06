c53d5b82d95df977bd861a6edc95ae6be1f25e6f
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-06 02:55:56 -0800

    Corrected file 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print("{}".format(matrix[i][j]), end="")
-            else:
-                print("{}".format(matrix[i][j]), end=" ")
-        print("")
+    if matrix is None:
+        return None
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if j == len(matrix[i]) - 1:
+                    print("{:d}".format(matrix[i][j]), end="")
+                else:
+                    print("{:d}".format(matrix[i][j]), end=" ")
+            print("")
