f0c1401abfa39e2cea1a6f51a0a2ee7e196c8b0f
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-10 04:57:45 -0800

    Added File 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary is None:
+        return None
+    else:
+        a_dictionary[key] = value
+        return a_dictionary
+
