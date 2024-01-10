64ac53dd74bbd235eab08184d8865f13ce95c7f3
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-10 05:58:43 -0800

    Rectified File 10 code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,5 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        maxim = next(iter(a_dictionary))
-        for i in a_dictionary:
-            if  i > maxim:
-                maxim = i
-        return maxim
+        my_keys = list(a_dictionary.keys())
+        return max(my_keys)
