9e7c83eb847670e870495b3ffc7009fb84c3947f
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-10 08:59:35 -0800

    pycodestyle correction

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,13 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    romanint = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romanint = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500, 'M': 1000}
     final = 0
     prev = 0
 
