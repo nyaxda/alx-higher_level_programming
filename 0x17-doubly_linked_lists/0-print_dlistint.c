b10c50240c478a5e1432393ef94d4e86542ba9ab
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-11 12:06:23 -0600

    Added Files 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    struct dlistint_s *current = head;
+    int count = 0;
+
+    while (current != NULL)
+    {
+        current = current->next;
+        count++;
+    }
+    return count;
+}
