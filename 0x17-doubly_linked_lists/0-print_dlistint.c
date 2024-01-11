feedd47384d7b64b88afe0c940e5d9a674680356
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-11 12:24:01 -0600

    Fix printing order in print_dlistint function

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,8 +12,8 @@ size_t print_dlistint(const dlistint_t *h)
 
     while (current != NULL)
     {
-        current = current->next;
         printf("%d\n", current->n);
+        current = current->next;
         count++;
     }
     return count;
