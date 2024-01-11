837feedc7cbd5661e5836ea2d236e3a7b4f72a22
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-11 12:17:58 -0600

    Fix missing semicolon in print_dlistint function

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,7 +13,7 @@ size_t print_dlistint(const dlistint_t *h)
     while (current != NULL)
     {
         current = current->next;
-        printf("%d\n", current->n)
+        printf("%d\n", current->n);
         count++;
     }
     return count;
