cdd2738cf9e44bd73d009b3603551512f379df7b
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-23 08:21:08 -0800

    Added 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""File that defines a square by size"""
+
+
+class Square:
+    """Class that defines a square by size"""
+    def __init__(self, size=0):
+        """_summary_
+        Args:
+        size (_type_): _description_
+        """
+        try:
+            size = int(size)
+        except TypeError:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("Size must be >= 0")
+            else:
+                self.__size = size
