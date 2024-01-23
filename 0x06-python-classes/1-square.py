313cc3d8a5b2070c2c6b99010d3b1dcb877e6803
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-23 07:54:17 -0800

    Added File 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""File that defines a square by size"""
+
+
+class Square:
+    """Class that defines a square by size"""
+    def __init__(self, size):
+        """_summary_
+        Args:
+        size (_type_): _description_
+        """
+        self.__size = size
