76cfececd6a4df3f458a306ca98a712d434dcd29
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-23 12:11:34 -0600

    Added File 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""File that defines a square by size"""
+
+
+class Square:
+    """Class that defines a square by size"""
+    def __init__(self, size=0, position=(0, 0):
+        """_summary_
+        Args:
+        size (_type_): _description_
+        """
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+     def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range (self.__position[1]):
+                print("")
+            for j in range(self.__size):
+                for e in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
+                
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, value):
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(a) is not int or type(b) is not int or a < 0 or b < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            a, b = value
+            self.__position = value
