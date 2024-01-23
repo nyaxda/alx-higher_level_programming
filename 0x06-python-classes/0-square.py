fb6bcf36d2f31159e43bf9fc9115d7ff82820d12
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-23 11:29:03 -0600

    Errorfix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-"""Filename: 0-square"""
+"""File that defines a square by size"""
 
 
 class Square:
-    """An empty class Square that defines a square"""
-    pass
+    """Class that defines a square by size"""
+    def __init__(self, size=0):
+        """_summary_
+        Args:
+        size (_type_): _description_
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -36,5 +36,3 @@ class Square:
                 for j in range(self.__size):
                     print("#", end="")
                 print("")
-
-            
