13b65e6edc3990dd9c2648b17652eb68d759b0f1
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-23 13:52:34 -0600

    Added docstrings

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 import math
-
+"""import math"""
 
 class _MagicClass:
+    """Magic class"""
     def __init__(self, radius=0):
+        """an init function"""
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
+        """area function"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """circumference function"""
         return 2 * math.pi * self.__radius
     
