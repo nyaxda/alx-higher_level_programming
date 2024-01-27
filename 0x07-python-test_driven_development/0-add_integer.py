a115043e525c207cde86640e627a4ec3f386f019
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-27 06:27:59 -0800

    Fix type error and formatting in add_integer.py and matrix_divided.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@
 def add_integer(a, b=98):
     """Adds two integers."""
     if not isinstance(a, (int, float)):
-        raise TypeError ("a must be an integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     c = a + b
