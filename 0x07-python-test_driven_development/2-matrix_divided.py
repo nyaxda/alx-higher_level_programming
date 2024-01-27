48f51c4ed603d29cb513c31a64d0bea457df4e69
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-27 03:47:56 -0800

    Refactor matrix_divided function to improve error handling

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,10 +14,11 @@ def matrix_divided(matrix, div):
     for i in matrix:
         if len(i) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
-        else:
-            for j in i:
-                if not isinstance(j, (int, float)):
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if not isinstance(i, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for j in i:
+            if not isinstance(j, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     matrix_2 = [[round(j / div, 2) for j in i] for i in matrix]
