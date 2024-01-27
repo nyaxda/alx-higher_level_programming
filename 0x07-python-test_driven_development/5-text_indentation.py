f3d7cb839634af0b89a5925bfabef72e590246d5
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-27 08:50:23 -0800

    Refactor text_indentation function to improve readability and formatting

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,11 +4,10 @@ def text_indentation(text):
     """Prints a text with 2 new lines after each of these characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    start = 0
-    for i, char in enumerate(text):
-        if char in ".?:":
-            final = (text[start:i + 1].strip())
-            if final:
-                print(final + "\n")
-            start = i + 1
-    print(text[start:].strip())
+    
+    text = text.replace('.', '.\n\n')
+    text = text.replace('?', '?\n\n')
+    text = text.replace(':', ':\n\n')
+    text = text.replace('  ', '')
+    text = text.replace('\n ', '\n')
+    print(text, end="")
