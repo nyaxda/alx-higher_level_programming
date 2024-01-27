8e5d3b6b00b7be75a6182bf9fc7a137a855aabfa
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-27 04:01:29 -0800

    Fix variable names in say_my_name function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,8 +2,8 @@
 
 def say_my_name(first_name, last_name=""):
     """prints My name is <first name> <last name>"""
-    if type(first name) is not str or first name is None:
+    if type(first_name) is not str or first name is None:
         raise TypeError("first_name must be a string")
-    if type(last name) is not str:
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
