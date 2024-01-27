45089c1018942d586d1775dda299a6b44ecb6b5e
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-27 07:12:31 -0800

    Added Files 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """Prints a square with the character #."""
+    if size is None:
+        raise TypeError("size must be an integer")
+    elif isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    elif not isinstance (size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
