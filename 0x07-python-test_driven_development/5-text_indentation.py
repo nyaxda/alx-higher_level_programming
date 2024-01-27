c140a3def82f15c9c8e384abc44f2dde14712fe0
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-27 08:44:40 -0800

    Refactor text_indentation function to improve readability and remove unnecessary print statements

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,8 +7,9 @@ def text_indentation(text):
     start = 0
     for i, char in enumerate(text):
         if char in ".?:":
-            print(text[start:i + 1].strip() + "\n")
-            if i != len(text) - 1:
-                print("\n")
+            final = (text[start:i + 1].strip())
+            if final:
+                print(final)
+                print()
             start = i + 1
     print(text[start:].strip())
