654ae175f8ca92ee8af88ee6a99c449779503ff5
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-27 04:01:48 -0800

    Fix typo in first_name variable check

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,7 +2,7 @@
 
 def say_my_name(first_name, last_name=""):
     """prints My name is <first name> <last name>"""
-    if type(first_name) is not str or first name is None:
+    if type(first_name) is not str or first_name is None:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
