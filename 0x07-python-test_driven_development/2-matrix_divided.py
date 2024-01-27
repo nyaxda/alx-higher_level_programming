a0492a986dee81ee7cec7ac04cf9bd0565bd0acb
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-27 03:36:35 -0800

    Fix matrix_divided function to handle division by zero and validate matrix input

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,11 +11,12 @@ def matrix_divided(matrix, div):
     for i in matrix:
         if not isinstance(i, list):
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if len(i) != len(matrix[0]):
+        elif len(i) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
-        for j in i:
-            if not isinstance(j, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        else:
+            for j in i:
+                if not isinstance(j, (int, float)):
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     matrix_2 = [[round(j / div, 2) for j in i] for i in matrix]
