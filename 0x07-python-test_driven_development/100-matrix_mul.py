982538daea930d09e9dd5148c8184276656ee8bd
Daniel Nyakundi <nyaxda@gmail.com>
2024-01-28 10:58:14 -0800

    Added File 100

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Module for matrix_mul method."""
+
+
+def matrix_mul(m_a, m_b):
+    """Multiply two matrices."""
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    elif len(m_a) == 0:
+        raise ValueError("m_a can't be empty")
+    else:
+        for row in m_a:
+            if not isinstance(row, list):
+                raise TypeError("m_a must be a list of lists")
+            elif len(row) == 0:
+                raise ValueError("m_a can't be empty")
+            elif len(row) != len(m_b[0]):
+                raise TypeError("each row of m_b must be of the same size")
+            elif len(row) != len(m_a[0]):
+                raise TypeError("each row of m_a must be of the same size")
+            else:
+                for num in row:
+                    if not isinstance(num, (int, float)):
+                        raise TypeError("m_a should contain only integers or floats")
+    
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    elif len(m_b) == 0:
+        raise ValueError("m_b can't be empty")
+    else:
+        for row in m_b:
+            if not isinstance(row, list):
+                raise TypeError("m_b must be a list of lists")
+            elif len(row) == 0:
+                raise ValueError("m_b can't be empty")
+            elif len(row) != len(m_b[0]):
+                raise TypeError("each row of m_b must be of the same size")
+            elif len(row) != len(m_b[0]):
+                raise TypeError("each row of m_b must be of the same size")
+            else:
+                for num in row:
+                    if not isinstance(num, (int, float)):
+                        raise TypeError("m_b should contain only integers or floats")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = []
+    for i in range(len(m_a)):
+        result_row = []
+        for j in range(len(m_b[0])):
+            sum = 0
+            for k in range(len(m_a[0])):
+                sum += m_a[i][k] * m_b[k][j]
+            result_row.append(sum)
+        result.append(result_row)
+
+    return result
