18f0c6c2dc8d6778e631976dbad7ce1f6a1816c6
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-05 08:08:00 -0800

    Remove unnecessary constructor in MyList class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,10 +4,6 @@
 
 class MyList(list):
     """inherits from list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__(self)
-
     def print_sorted(self):
         """prints the list, but sorted"""
         print(sorted(self))
