c8df1c0bf1f20ee0ee088b0625232c41cec1bd2e
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-05 09:17:30 -0800

    Refactor integer_validator method to include detailed documentation and additional test cases

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,10 +17,17 @@ class BaseGeometry:
     
     def integer_validator(self, name, value):
         """
-        This method validates the value
+        This method validates the value passed to it
+
+        Arguments:
+        name (str): the name of the value
+        value (int): the value to validate
+
+        Raises:
+        TypeError: if value is not an integer
+        ValueError: if value is less than or equal to 0
         """
         if type(value) is not int:
             raise TypeError(name + " must be an integer")
         if value <= 0:
             raise ValueError(name + " must be greater than 0")
-
