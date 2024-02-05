e1de3a347010450adf901e8d16ec317dab197872
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-05 09:39:34 -0800

    Implement area() method and fix indentation

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
         """
 
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """
         This method validates the value passed to it
