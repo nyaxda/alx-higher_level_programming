0361b6db7c6c2b5ae44ab4ce4bd98bb8ca00c592
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-05 08:46:14 -0800

    Add additional test cases for print_sorted() method

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,3 +7,4 @@ def is_same_class(obj, a_class):
     """This function returns True if the object is exactly an instance
     of the specified class, otherwise False"""
     return type(obj) == a_class
+
