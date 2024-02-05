779108853e2076392084321db6fc0535a8dcfd0c
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-05 08:58:23 -0800

    Update user authentication logic to use bcrypt hashing algorithm

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    This class is the base class for geometry objects
+    """
+
+    def area(self):
+        """
+        This method raises an exception
+        """
+        raise Exception("area() is not implemented")
+
