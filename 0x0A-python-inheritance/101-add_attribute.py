462aa875f897bed4925fea906daf233bb4084241
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-05 10:31:17 -0800

    Add attribute function with error handling

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,16 @@
 
 
 def add_attribute(obj, attr_name, attr_value):
+     """Add a new attribute to an object if possible.
+
+    Args:
+        obj (any): The object to add an attribute to.
+        att (str): The name of the attribute to add to obj.
+        value (any): The value of att.
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+
     if hasattr(obj, '__dict__'):
         setattr(obj, attr_name, attr_value)
     else:
