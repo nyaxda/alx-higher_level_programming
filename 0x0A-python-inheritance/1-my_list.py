8bd3121c8c6f3b6b9e1df87aa0508cd43d14effc
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-05 08:04:45 -0800

    Add __init__ method to MyList class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,9 @@
 
 class MyList(list):
     """inherits from list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__(self)
 
     def print_sorted(self):
         """prints the list, but sorted"""
