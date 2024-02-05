9417db0eb7461d039f837e91f776dc09c0be2069
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-05 08:54:36 -0800

    Refactor MyList class to include a print_sorted method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""This module contains a class that inherits from list"""
+"""
+This module contains a class that inherits from list
+"""
 
 
 class MyList(list):
-    """inherits from list"""
+    """
+    inherits from list and has a method that prints the list,
+     but sorted
+    """
+    
     def print_sorted(self):
         """
             prints the list, but sorted
