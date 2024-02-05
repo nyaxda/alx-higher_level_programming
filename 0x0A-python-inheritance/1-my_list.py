b84678580141667dcf08b3a597645fe0a92d1f11
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-05 07:02:03 -0800

    Added File 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a class that inherits from list"""
+
+
+class MyList(list):
+    """inherits from list"""
+    def print_sorted(self):
+        """prints the list, but sorted"""
+        print(sorted(self))
