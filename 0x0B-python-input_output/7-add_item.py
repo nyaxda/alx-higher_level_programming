c4d43f39b49bf5457386dc76bb4929f79359aa5c
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 15:41:59 -0800

    Added File 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that adds all arguments to a Python list,
+and then save them to a file"""
+import sys
+
+savejsonfile = __import__('5-save_to_json_file').save_to_json_file
+loadjsonfile = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+if not os.path.isfile(filename):
+    save_to_json_file([], filename)
+
+items = load_from_json_file(filename)
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
