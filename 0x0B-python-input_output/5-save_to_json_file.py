e0ba2726b66396f25a2d0a4127789cdfb50828aa
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 15:15:22 -0800

    Fix save_to_json_file function to write JSON data to file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,4 +14,4 @@ def save_to_json_file(my_obj, filename):
     """
 
     with open(filename, 'w', encoding='utf8') as f:
-        return str(json.dumps(my_obj))
+        return f.write(str(json.dumps(my_obj)))
