417fefa72fb153f79c544598e12ea711961aaddb
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 14:38:47 -0800

    Fix from_json_string function

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -6,9 +6,9 @@ import json
 def from_json_string(my_str):
     """
         this function returns an object represented by a JSON string
-        
+
         Args:
             obj: JSON string representation to be loaded
     """
 
-    json.loads(obj)
+    return json.loads(my_str)
