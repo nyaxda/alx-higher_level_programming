5ceedf68a34fcca36fc01a2abfb1968ec78385d6
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 15:53:02 -0800

    Refactor script to load, add, and save arguments from Command Line

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list,
-and then save them to a file"""
+""" Load, add, Save arguments given in the Command Line"""
+
 import sys
 
+
 if __name__ == '__main__':
+    load_from_json_file = \
+            __import__('6-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
