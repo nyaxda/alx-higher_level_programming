60aca5ceb1a7dd28d9d5a53429b805267c8e9cf8
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 14:37:39 -0800

    Added File 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""returns an object represented by a JSON string"""
+import json
+
+
+def from_json_string(my_str):
+    """
+        this function returns an object represented by a JSON string
+        
+        Args:
+            obj: JSON string representation to be loaded
+    """
+
+    json.loads(obj)
