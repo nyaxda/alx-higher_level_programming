20fba9a6de509906cf8cfe296362ecc95614ad98
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 15:45:16 -0800

    Fix indentation and handle FileNotFoundError in 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
     savejsonfile = __import__('5-save_to_json_file').save_to_json_file
     loadjsonfile = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+    filename = "add_item.json"
 
-if not os.path.isfile(filename):
-    save_to_json_file([], filename)
-
-items = load_from_json_file(filename)
-items.extend(sys.argv[1:])
-save_to_json_file(items, filename)
+    try:
+        items = load_from_json_file(filename)
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
