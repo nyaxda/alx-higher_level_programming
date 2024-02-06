eca2b368cfa39fcb55afacaf4dcdbafb80e18fe5
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 15:59:47 -0800

    Refactor add_item.py to handle existing JSON file and add command line arguments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,16 +2,15 @@
 """ Load, add, Save arguments given in the Command Line"""
 
 import sys
+from os import path
 
-
-if __name__ == '__main__':
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+if path.exists('add_item.json'):
+    items = load_from_json_file('add_item.json')
+else
+    items = []
+for i in range(1, len(sys.argv))
+    items.append(sys.argv[i])
+save_to_json_file(items, 'add_item.json')
     
