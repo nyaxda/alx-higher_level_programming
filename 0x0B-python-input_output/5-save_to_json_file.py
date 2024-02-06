bba07e0dd2262b3917638c69244eaf8fc5704e12
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 14:47:14 -0800

    Added File 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""writes an Object to a text file, using a JSON representation"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+        this function writes an Object to a text file
+        using a JSON representation
+
+        Args:
+            my_obj: JSON represented string
+            filename: file to be written to
+    """
+
+    def save_to_json_file(my_obj, filename):
+        representation = str(my_obj)
+        with open(filename, 'w', encoding='utf8'):
+            return f.write(representation)
