d35d255c45ccb52e31f9a87789a9ee52db66d2e2
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 16:00:01 -0800

    Fix syntax error in add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 if path.exists('add_item.json'):
     items = load_from_json_file('add_item.json')
-else
+else:
     items = []
 for i in range(1, len(sys.argv))
     items.append(sys.argv[i])
