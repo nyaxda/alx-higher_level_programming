4ba9882a10abb199a8c2bc2d90c9668518cfdea7
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 16:05:08 -0800

    Refactor add_item.py to load, add, and save arguments from Command Line

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""adds arguements to a Python list then
-saves it to a file
-"""
-from os import path
+""" Load, add, Save arguments given in the Command Line"""
+
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if path.exists('add_item.json'):
-    obj_json_file = load_from_json_file('add_item.json')
-else:
-    obj_json_file = []
 
-for i in range(1, len(sys.argv)):
-    obj_json_file.append(sys.argv[i])
+if __name__ == '__main__':
+    load_from_json_file = \
+            __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-save_to_json_file(obj_json_file, 'add_item.json')
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
