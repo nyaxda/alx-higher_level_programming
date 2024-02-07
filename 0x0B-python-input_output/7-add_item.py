886f21dad18d7c404b83c47ab7dbb4655a850987
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 16:03:40 -0800

    Fix argument handling and variable name in 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" Load, add, Save arguments given in the Command Line"""
-
-import sys
+"""adds arguements to a Python list then
+saves it to a file
+"""
 from os import path
-
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 if path.exists('add_item.json'):
     items = load_from_json_file('add_item.json')
 else:
     items = []
+
 for i in range(1, len(sys.argv)):
-    items.append(sys.argv[i])
+    obj_json_file.append(sys.argv[i])
+
 save_to_json_file(items, 'add_item.json')
