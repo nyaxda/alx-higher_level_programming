b8c75da1978e119914146e5313bf19f7373e5f8a
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 16:04:26 -0800

    Refactor add_item.py to use descriptive variable names

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if path.exists('add_item.json'):
-    items = load_from_json_file('add_item.json')
+    obj_json_file = load_from_json_file('add_item.json')
 else:
-    items = []
+    obj_json_file = []
 
 for i in range(1, len(sys.argv)):
     obj_json_file.append(sys.argv[i])
 
-save_to_json_file(items, 'add_item.json')
+save_to_json_file(obj_json_file, 'add_item.json')
