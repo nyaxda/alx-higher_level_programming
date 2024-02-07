8805cca0b37b85ddd194e9fb0fe50227b618179a
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 16:14:49 -0800

    Add import statement for os module

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,7 @@
 """script that adds all arguments to a Python list,
 and then save them to a file"""
 import sys
-
+import os
 savejsonfile = __import__('5-save_to_json_file').save_to_json_file
 loadjsonfile = __import__('6-load_from_json_file').load_from_json_file
 
