1b2cbcd38be3b9b2090c9b5597c3bfb84d04aad9
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-06 16:12:51 -0800

    Refactor script to add command line arguments to a Python list and save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-""" Load, add, Save arguments given in the Command Line"""
-
+"""script that adds all arguments to a Python list,
+and then save them to a file"""
 import sys
 
+savejsonfile = __import__('5-save_to_json_file').save_to_json_file
+loadjsonfile = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == '__main__':
-    load_from_json_file = \
-            __import__('6-load_from_json_file').load_from_json_file
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+filename = "add_item.json"
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+if not os.path.isfile(filename):
+    save_to_json_file([], filename)
+items = load_from_json_file(filename)
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
