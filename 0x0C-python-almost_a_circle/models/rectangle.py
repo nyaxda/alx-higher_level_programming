c0884def99a643bb04bbf8f51ccca90747ead75a
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-11 00:58:38 -0800

    Fix formatting in Rectangle's __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,5 +84,5 @@ class Rectangle(Base):
             print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(id, x, y, width, height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
     
