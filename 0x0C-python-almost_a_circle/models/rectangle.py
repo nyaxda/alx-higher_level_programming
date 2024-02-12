ef6f75e7b44cc4492999f845638f6322c47e7c5a
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-12 11:35:46 -0800

    Fix type check in Rectangle setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,7 +20,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Width setter method."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
@@ -35,7 +35,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Height setter method"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value <= 0:
             raise ValueError("height must be > 0")
@@ -50,7 +50,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """x setter method."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         elif value < 0:
             raise ValueError("x must be >= 0")
@@ -65,7 +65,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """y setter method."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         elif value < 0:
             raise ValueError("y must be >= 0")
