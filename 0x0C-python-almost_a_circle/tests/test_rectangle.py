52f50d468e8ad585795d839250458b9a42e24f36
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-12 09:51:16 -0800

    pycodestyle 3

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -178,22 +178,20 @@ class TestRectangle(unittest.TestCase):
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
         Rectangle.save_to_file([r1, r2])
-        expected_output = [
-    {
-        "y": 8, 
-        "x": 2,
-        "id": 1,
-        "width": 10,
-        "height": 7
-    },
-    {
-        "y": 0, 
-        "x": 0,
-        "id": 2,
-        "width": 2,
-        "height": 4
-    }
-]
+        expected_output = [{
+            "y": 8, 
+            "x": 2,
+            "id": 1,
+            "width": 10,
+            "height": 7
+        },
+        {
+            "y": 0, 
+            "x": 0,
+            "id": 2,
+            "width": 2,
+            "height": 4
+        }]
         with open("Rectangle.json", "r") as file:
             content = json.load(file)
             self.assertEqual(content, expected_output)
