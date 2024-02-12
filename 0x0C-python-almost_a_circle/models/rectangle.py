fa8b5f36f6c0721307988f9da1443cbbd377acf9
Daniel Nyakundi <nyaxda@gmail.com>
2024-02-12 08:41:46 -0800

    Fix string representation of Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,8 +92,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """string representation of the rectangle"""
-        return "[Rectangle] ({}) {}/{} - {}/{}"
-    .format(self.id, self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
         """Assigns arguments to each attribute."""
