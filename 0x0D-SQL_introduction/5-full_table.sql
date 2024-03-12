8d7716f8c358d8f446f51a4d129928bae2d4864a
Daniel Nyakundi <nyaxda@gmail.com>
2024-03-12 16:50:55 -0700

    Added File 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 USE `hbtn_0c_0`
-SELECT * FROM `first_table`;
+SHOW CREATE TABLE `first_table`;
