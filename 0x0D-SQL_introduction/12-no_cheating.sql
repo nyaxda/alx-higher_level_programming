2e4a177ec6c5cf8df1eb742aac235558d9e9a03b
Daniel Nyakundi <nyaxda@gmail.com>
2024-03-12 17:43:30 -0700

    Fix score update query in 12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,7 @@
 -- updates the score of Bob to 10 in the table second_table.
 UPDATE `second_table`
-SET 'score' = 10
-WHERE `name` = "Bob";
+SET `score` = 10
+WHERE `id` =        
+        (SELECT `id`
+            FROM `second_table`
+            WHERE name = 'Bob');
