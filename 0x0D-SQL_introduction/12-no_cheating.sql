17d3197821994e86ff77a0f24498c2e323d33b1a
Daniel Nyakundi <nyaxda@gmail.com>
2024-03-12 17:38:25 -0700

    Fix SQL syntax in 12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,7 +1,7 @@
 -- updates the score of Bob to 10 in the table second_table.
 UPDATE `second_table`
 SET 'score' = 10
-WHERE id =        
+WHERE `id =        
         (SELECT `id`
             FROM `second_table`
-            WHERE name = 'Bob')
+            WHERE name = 'Bob');
