e3be91bd9e313a780d09b35512f26d03afdb8bc3
Daniel Nyakundi <nyaxda@gmail.com>
2024-03-13 16:42:13 -0700

    Errorfix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name AS city_name, states.name AS state_name
 FROM cities
-    INNER JOIN states
+    RIGHT JOIN states
     ON cities.state_id = states.id
 ORDER BY cities.id ASC;
