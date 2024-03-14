28d24c9d4fb27a8f901c64bf50bd03ee0df88047
Daniel Nyakundi <nyaxda@gmail.com>
2024-03-13 17:19:04 -0700

    Errorfix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre.id ASC;
+ORDER BY tv_shows.title ASC, tv_shows_genres.genre.id ASC;
