fe9c08a15bf6975414f9fb9198e109d09eae45b0
NyaxDa <nyaxda@gmail.com>
2024-04-09 09:47:48 -0700

    Added File 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-/**
- * script that imports an array and computes a new array
- */
-const list = require('./100-data').list;
+const list = require('./100-data.js').list;
 
-const map1 = list.map((x, index) => x * index);
+const newList = list.map((val, idx) => val * idx);
 console.log(list);
-console.log(map1);
+console.log(newList);
