b7704b8e15bdf0a05d849c17e833ec26e477faaf
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-09 10:05:22 -0700

    Added Files 102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * script that concats 2 files.
+ */
+const fs = require('fs');
+
+let file1 = fs.readFileSync(process.argv[2], 'utf8');
+let file2 = fs.readFileSync(process.argv[3], 'utf8');
+
+fs.writeFileSync(process.argv[4], file1 + file2);
