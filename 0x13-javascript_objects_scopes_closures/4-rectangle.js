8c3eda1d17812866d90495e4b4e5fbad29c9f182
NyaxDa <nyaxda@gmail.com>
2024-04-09 08:16:46 -0700

    Added File 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/**
+ * rectangle Class
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
