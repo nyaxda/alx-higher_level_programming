bd067a1b15f5713449f7f103b3b63c7b4d3be1f1
NyaxDa <nyaxda@gmail.com>
2024-04-09 08:03:16 -0700

    Added Files 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/**
+ * rectangle Class
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+}
+
+module.exports = Rectangle;
