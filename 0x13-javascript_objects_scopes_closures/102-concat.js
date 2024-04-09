6ec4eefe24cd284982591194747136e8a0a45111
NyaxDa <nyaxda@gmail.com>
2024-04-09 10:08:44 -0700

    Made the file semistandard compliant

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,7 +4,7 @@
  */
 const fs = require('fs');
 
-let file1 = fs.readFileSync(process.argv[2], 'utf8');
-let file2 = fs.readFileSync(process.argv[3], 'utf8');
+const file1 = fs.readFileSync(process.argv[2], 'utf8');
+const file2 = fs.readFileSync(process.argv[3], 'utf8');
 
 fs.writeFileSync(process.argv[4], file1 + file2);
