571619bab580adf8558d515cc90a74762af2f870
NyaxDa <nyaxda@gmail.com>
2024-04-09 07:45:33 -0700

    Added Files 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -6,11 +6,10 @@ class Rectangle {
   width;
   height;
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return;
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 
