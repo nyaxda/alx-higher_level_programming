665e4faac3be3a1da7eae118ed725e16f377df4b
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-09 07:16:31 -0700

    Added File 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * rectangle Class
+ */
+class Rectangle {
+  width;
+  height;
+  constructor (width, height) {
+    this.w = width;
+    this.h = height;
+  }
+}
+
+module.exports = Rectangle;
