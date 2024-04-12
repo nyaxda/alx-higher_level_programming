3972b84466509b05809d7f9bfd67801856381915
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-12 16:30:35 -0700

    Refactor model_state.py for improved readability and formatting

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
-                ORDER BY ID ASC""")
+    ORDER BY ID ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
