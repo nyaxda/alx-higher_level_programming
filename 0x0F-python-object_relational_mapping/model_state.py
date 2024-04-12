ab763dfa9598ab004c2e57ca240db5d960766136
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-12 16:40:00 -0700

    Refactor model_state.py to define State class and link it to the MySQL table 'states'

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """State class that links to MySQL table states"""
     __tablename__ = 'states'
