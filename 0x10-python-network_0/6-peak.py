c6578aa7cf934026f49d304b97e0ab98fd5f1916
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-25 13:53:04 -0700

    Added Interview Question 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function that finds a
+peak in a list of unsorted integers.
+"""
+
+
+def find_peak(list_of_integers):
+    """Function that finds a peak in a list of unsorted integers."""
+    if not list_of_integers:
+        return None
+    max = list_of_integers[0]
+    for i in range(len(list_of_integers)):
+        if list_of_integers[i] > max:
+            max = list_of_integers[i]
+    return max
