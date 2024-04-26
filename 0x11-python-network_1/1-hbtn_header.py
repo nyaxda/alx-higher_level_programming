49042c87caa1a2b00bff073a06d1d75f8eb1497a
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-26 14:34:34 -0700

    Update X-Request-Id header retrieval in 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+        print(response.headers.get('X-Request-Id'))
