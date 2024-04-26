068391d90e6fa6ec9883b7467fe6c3999e2c20c3
NyaxDa <nyaxda@gmail.com>
2024-04-26 16:51:45 -0700

    Added File 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     commits = r.json()
     try:
         for i in range(10):
-            print("{}: {}".format(commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+            print("{}: {}".format(commits[i].get("sha"), commits[i].get(
+                "commit").get("author").get("name")))
     except IndexError:
-                pass
+        pass
