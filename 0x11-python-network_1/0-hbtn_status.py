2fe9451825d9cf72d6ed55094760cd748133f4ef
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-26 14:04:50 -0700

    Update print statements in 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,8 +2,10 @@
 """
 Script that fetches https://intranet.hbtn.io/status
 """
+import urllib.request
+
+
 if __name__ == '__main__':
-    import urllib.request
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
