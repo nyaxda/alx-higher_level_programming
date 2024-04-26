d676b77b738796c9c5a8e1c0c659bddb9efe0834
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-26 13:54:05 -0700

    Update print statements in 0-hbtn_status.py and fix shebang in 6-peak.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
-    print("   - type: {}". format(type(html)))
-    print("   - content: {}". format(html))
-    print("   - utf8 content: {}".format(html.decode('utf-8')))
+    print("    - type: {}". format(type(html)))
+    print("    - content: {}". format(html))
+    print("    - utf8 content: {}".format(html.decode('utf-8')))
